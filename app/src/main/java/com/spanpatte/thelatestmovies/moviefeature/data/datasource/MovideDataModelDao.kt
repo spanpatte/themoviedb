4c6f7d2e3e799f64653cf41db8577cf10a2e45ee
Satish Panpatte <satish.panpatte@hilti.com>
2023-06-18 18:15:00 +0530

    Movies limit set to 10

--- app/src/main/java/com/spanpatte/thelatestmovies/moviefeature/data/datasource/MovideDataModelDao.kt
+++ app/src/main/java/com/spanpatte/thelatestmovies/moviefeature/data/datasource/MovideDataModelDao.kt
@@ -7,7 +7,7 @@ import com.spanpatte.thelatestmovies.moviefeature.data.models.MovieDataModel
 
 @Dao
 interface MovieDataModelDao {
-    @Query("SELECT * FROM MovieDataModel")
+    @Query("SELECT * FROM MovieDataModel LIMIT 0,10")
     fun getAll(): List<MovieDataModel>
 
     @Insert
